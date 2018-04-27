4ba2715e3b58390ea835e1cfe4cdc37bda1bb6a8
Lopez Oderda, Lucas F <Lucas.F.Lopez.Oderda@kcc.com>
2018-04-27 18:58:50 -0300

    API Okta

---
+++ IDaaS/APIOkta/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace APIOkta
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<JwtBearerOptions>(Configuration.GetSection("Authentication:Okta"));
+
+            var serviceProvider = services.BuildServiceProvider();
+            var authOptions = serviceProvider.GetService<IOptions<JwtBearerOptions>>();
+
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Authority = authOptions.Value.Authority;
+                options.Audience = authOptions.Value.Audience;
+            });
+
+            services.AddMvc();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
--- IDaaS/WebAppOkta/Pages/About.cshtml.cs
+++ IDaaS/WebAppOkta/Pages/About.cshtml.cs
@@ -4,15 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http;
+using System.Net;
+
 namespace WebAppOkta.Pages
 {
     public class AboutModel : PageModel
     {
         public string Message { get; set; }
 
-        public void OnGet()
+        private static HttpClient client = new HttpClient();
+        private static string baseURL = "http://localhost:59569";
+
+        public async Task OnGetAsync()
         {
-            Message = "Your application description page.";
+            if (User.Identity.IsAuthenticated)
+            {
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+
+                var request = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri(baseURL + "/api/token"),
+                    Method = HttpMethod.Get
+                };
+                request.Headers.TryAddWithoutValidation("Authorization", "Bearer " + accessToken);
+
+                var result = client.SendAsync(request).Result;
+
+                Message = result.StatusCode == HttpStatusCode.OK ? result.Content.ReadAsStringAsync().Result : "Error HTTP Code: " + result.StatusCode;
+            }
         }
     }
 }
